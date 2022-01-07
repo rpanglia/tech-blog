a540188e40eeb708529e0d54193aeb0185876319
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-06 20:31:00 -0500

    models/posts + comments.js updated and controllers/api/user-routes started

---
+++ models/Posts.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create  post model
+class Post extends Model {}
+
+// define fields/columns (id, title, post-content, user-id) 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post; 
