e89b51da3f54ae64a3131ea440a318f3f58319e6
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-08 22:08:19 -0500

    edit-post.js and new js files added under public folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const mainHomeRoutes = require('./mainhome-routes.js');
+const mainHomeRoutes = require('./mainhome-routes');
 const dashboardRoutes = require('./dash-routes');
 
 router.use('/', mainHomeRoutes);
---
+++ public/js/edit-post.js
@@ -0,0 +1,35 @@
+async function editFormHandler(event) {
+    
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_content = document.querySelector('input[name="post-content"]').value.trim();
+
+    const id = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+    ];
+
+    console.log(title);
+    console.log(post_content);
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+            post_content,
+            title
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    console.log(response);
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector(".edit-post-form")
+.addEventListener("submit", editFormHandler);
