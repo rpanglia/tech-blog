6f804bdc7957ea66209a38216d8e5a1e3780b298
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-09 13:05:35 -0500

    fixing connection.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const routes = require('./controllers');
+const routes = require('./controllers/');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -23,6 +23,7 @@ const sess = {
         db: sequelize
     })
 };
+
 app.use(session(sess));
 app.use(routes);
 
